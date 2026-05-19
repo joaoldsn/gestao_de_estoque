13beb96d56eede14de0c48a2695728dd7e401839
Luiz-Antonio-Xavier <luizxavier000s@gmail.com>
2026-05-19 20:59:57 -0300

    Criação do arquivo menu.py

---
+++ menu.py
@@ -0,0 +1,19 @@
+def exibir_menu():
+    print("""
+┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+┃          SISTEMA DE GESTÃO DE ESTOQUE            ┃
+┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+                MENU PRINCIPAL
+
+   [1] Cadastrar Produto
+   [2] Entrada de Estoque
+   [3] Saída de Estoque
+   [4] Consultar Produto
+   [5] Listar Produtos
+
+   [0] Encerrar Sistema
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+""")
+    
---
+++ test.py
@@ -0,0 +1,14 @@
+from menu import exibir_menu
+
+while True:
+
+    exibir_menu()
+
+    opcao = input("Escolha uma opção: ")
+
+    if opcao == "1":
+        print("Cadastrar produto")
+
+    elif opcao == "0":
+        print("Sistema encerrado.")
+        break
