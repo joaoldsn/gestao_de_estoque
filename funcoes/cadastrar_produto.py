448f8e089459262ea0830a8608040c964d73201c
vinicius-oliveira27 <vini.voaaraujoooo@ggmail.com>
2026-05-25 16:55:37 -0300

    feat: adicionar "categoria" a função cadastrar de produto

--- funcoes/cadastrar_produto.py
+++ funcoes/cadastrar_produto.py
@@ -1,12 +1,15 @@
 def cadastrar_produto(nome, preco, quantidade):
     nome = input('Digite o nome do produto: ')
+    categoria = input('Digite a categoria do produto: ')
     quantidade=int(input('Digite a quantidade do produto: '))
     preco=float(input('Digite o preço do produto: '))
     produto = {
         'nome': nome,
+        'categoria': categoria,
         'quantidade': quantidade,
         'preço': preco,
     }
 
         estoque.append(produto)
         print('Produto cadastrado com sucesso!')
+
