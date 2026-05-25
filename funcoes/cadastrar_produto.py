2a9aa4ee1e3fe201a123984f3d4e06d5665f275d
vinicius-oliveira27 <vini.voaaraujoooo@ggmail.com>
2026-05-25 20:19:05 -0300

    feat: adicionar loop para melhorar fluxo

--- funcoes/cadastrar_produto.py
+++ funcoes/cadastrar_produto.py
@@ -1,24 +1,26 @@
 def cadastrar_produto(nome, categoria, preco, quantidade):
-    nome = input('Digite o nome do produto: ')
-    categoria = input('Digite a categoria do produto: ')
-    quantidade=int(input('Digite a quantidade do produto: '))
-    preco=float(input('Digite o preço do produto: '))
-    produto = {
-        'nome': nome,
-        'categoria': categoria,
-        'quantidade': quantidade,
-        'preço': preco,
+    while True:
+
+      nome = input('Digite o nome do produto: ')
+      quantidade=int(input('Digite a quantidade do produto: '))
+      preco=float(input('Digite o preço do produto: '))
+      produto = {
+          'nome': nome,
+          'categoria': categoria,
+          'quantidade': quantidade,
+          'preço': preco,
       }
 
-    decisao=input('Confirmar cadastro? (sim/não)')
-    if decisao == 'sim':
-      estoque.append(produto)
-      print('Produto cadastrado com sucesso!')
+      decisao=input('Confirmar cadastro? (sim/não)')
+      if decisao == 'sim':
+          estoque.append(produto)
+          print('Produto cadastrado com sucesso!')
+          break
 
-    elif decisao == 'não':
+      elif decisao == 'não':
 
-      print('Cadastro cancelado. Digite os dados para um novo produto.')
+          print('Cadastro cancelado. Digite os dados para um novo produto.')
 
-    else:
-      print('Resposta inválida. Por favor, digite "sim" ou "não".')
+      else:
+          print('Resposta inválida. Por favor, digite "sim" ou "não".')
 
