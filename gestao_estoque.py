22b3dc43f62a3ea64cf52a9fb174ccb1d16600a7
joaoldsn <joaolsn4@gmail.com>
2026-05-25 17:57:16 -0300

    feat: remoção da conexão com o banco de dados e simplificação do código

--- gestao_estoque.py
+++ gestao_estoque.py
@@ -1,23 +1,6 @@
-import sqlite3
-import os
+from funcoes.menu import menu
+from funcoes.conexao import conexao
 
-caminho_banco = os.path.join(os.path.dirname(__file__), "estoque.db")
+menu()
 
-conexao = sqlite3.connect(caminho_banco)
-cursor = conexao.cursor()
-
-def criar_tabela():
-    cursor.execute("""
-    CREATE TABLE IF NOT EXISTS produtos (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        nome TEXT NOT NULL,
-        categoria TEXT NOT NULL,
-        preco REAL NOT NULL,
-        quantidade INTEGER NOT NULL
-    )
-    """)
-
-    conexao.commit()
-
-criar_tabela()
 conexao.close()
