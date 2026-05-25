e2c15926f79a6a96d8a10d21404ef2ab87caf132
Luiz-Antonio-Xavier <luizxavier000s@gmail.com>
2026-05-25 20:49:59 -0300

    feat: implementar função para registrar entrada

--- funcoes/registrar_entrada.py
+++ funcoes/registrar_entrada.py
@@ -1,2 +1,31 @@
-def registrar_entrada(produto_id, quantidade):
-    """Adiciona itens ao estoque"""
+from funcoes.conexao import conexao, cursor
+
+def registrar_entrada():
+
+    nome = input('\nDigite o nome do produto: ')
+    quantidade = int(input('Digite a quantidade de entrada: '))
+
+    cursor.execute("""
+    SELECT quantidade FROM produtos
+    WHERE nome = ?
+    """, (nome,))
+
+    produto = cursor.fetchone()
+
+    if produto == None:
+
+        print('\n Produto não encontrado.')
+        return
+
+    nova_quantidade = produto[0] + quantidade
+
+    cursor.execute("""
+    UPDATE produtos
+    SET quantidade = ?
+    WHERE nome = ?
+    """, (nova_quantidade, nome))
+
+    conexao.commit()
+
+    print('\n Entrada registrada com sucesso!')
+from funcoes.conexao import conexao, cursor
