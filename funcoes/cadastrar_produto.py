3499bc7c2d9479b60f70483fe27838932e543e0f
vinicius-oliveira27 <vini.voaaraujoooo@ggmail.com>
2026-05-25 16:42:08 -0300

    feat: refactor função de cadastrar

--- funcoes/cadastrar_produto.py
+++ funcoes/cadastrar_produto.py
@@ -1,2 +1,12 @@
-def cadastrar_produto(nome, categoria, preco, quantidade):
-    """Registra novo produto no sistema"""
+def cadastrar_produto(nome, preco, quantidade):
+    nome = input('Digite o nome do produto: ')
+    quantidade=int(input('Digite a quantidade do produto: '))
+    preco=float(input('Digite o preço do produto: '))
+    produto = {
+        'nome': nome,
+        'quantidade': quantidade,
+        'preço': preco,
+    }
+
+        estoque.append(produto)
+        print('Produto cadastrado com sucesso!')
