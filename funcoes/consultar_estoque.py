df7045d34e9c80f3cd92bfb0940c2b48207b8a48
joaoldsn <joaolsn4@gmail.com>
2026-05-25 20:40:12 -0300

    feat: implementar função de consulta de estoque por nome do produto

--- funcoes/consultar_estoque.py
+++ funcoes/consultar_estoque.py
@@ -1,2 +1,24 @@
-def consultar_estoque(produto_id):
-    """Verifica saldo atual do produto"""
+from funcoes.conexao import cursor
+
+def consultar_estoque():
+
+    nome = input('\nDigite o nome do produto: ')
+
+    cursor.execute("""
+    SELECT * FROM produtos
+    WHERE nome = ?
+    """, (nome,))
+
+    produto = cursor.fetchone()
+
+    if produto is None:
+
+        print('\n❌ Produto não encontrado.')
+        return
+
+    print('\n===== DADOS DO PRODUTO =====')
+    print(f'ID: {produto[0]}')
+    print(f'Nome: {produto[1]}')
+    print(f'Categoria: {produto[2]}')
+    print(f'Preço: R$ {produto[3]:.2f}')
+    print(f'Quantidade: {produto[4]}')
