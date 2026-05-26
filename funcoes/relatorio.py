5bb7920cad0b5b5d610bc531939af7f2b1fa6a4c
vinicius-oliveira27 <vini.voaaraujoooo@gmail.com>
2026-05-25 21:16:43 -0300

    feat: implementar função de relatório de produtos

---
+++ funcoes/relatorio.py
@@ -0,0 +1,25 @@
+from funcoes.conexao import cursor
+
+def relatorio():
+
+    cursor.execute("""
+    SELECT * FROM produtos
+    """)
+
+    produtos = cursor.fetchall()
+
+    if produtos == []:
+
+        print('\n📦 Nenhum produto cadastrado.')
+        return
+
+    print('\n===== RELATÓRIO =====')
+
+    for produto in produtos:
+
+        print('\n-------------------')
+        print(f'ID: {produto[0]}')
+        print(f'Nome: {produto[1]}')
+        print(f'Categoria: {produto[2]}')
+        print(f'Preço: R$ {produto[3]:.2f}')
+        print(f'Quantidade: {produto[4]}')
