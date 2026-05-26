532cee4da6b25f7dbfae2396207f6d902448cf09
vinicius-oliveira27 <vini.voaaraujoooo@gmail.com>
2026-05-25 21:17:36 -0300

    feat: refactor: atualizar menu principal com opções de registro e consulta e reorganização

--- funcoes/menu.py
+++ funcoes/menu.py
@@ -1,4 +1,13 @@
-def exibir_menu():
+from funcoes.cadastrar_produto import cadastrar_produto
+from funcoes.registrar_entrada import registrar_entrada
+from funcoes.registrar_saida import registrar_saida
+from funcoes.consultar_estoque import consultar_estoque
+from funcoes.alerta import alertar_estoque_baixo
+from funcoes.relatorio import relatorio
+
+def menu():
+
+    while True:
     print("""
 ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
 ┃          SISTEMA DE GESTÃO DE ESTOQUE            
@@ -6,14 +15,46 @@ def exibir_menu():
 
                 MENU PRINCIPAL
 
-   [1] Cadastrar Produto
-   [2] Entrada de Estoque
-   [3] Saída de Estoque
-   [4] Consultar Produto
-   [5] Listar Produtos
+    1 - Cadastrar produto
+    2 - Registrar entrada
+    3 - Registrar saída
+    4 - Consultar estoque
+    5 - Alerta de estoque baixo
+    6 - Relatório
+    0 - Sair
+    """)
+        opcao = input('\nEscolha uma opção: ')
 
-   [0] Encerrar Sistema
+        if opcao == '1':
 
-━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-""")
+            cadastrar_produto()
+
+        elif opcao == '2':
+
+            registrar_entrada()
+
+        elif opcao == '3':
+
+            registrar_saida()
+
+        elif opcao == '4':
+
+            consultar_estoque()
+
+        elif opcao == '5':
+
+            alertar_estoque_baixo()
+
+        elif opcao == '6':
+
+            relatorio()
+
+        elif opcao == '0':
+
+            print('\n Sistema encerrando...')
+            break
+
+        else:
+
+            print('\n Opção inválida.')
     
